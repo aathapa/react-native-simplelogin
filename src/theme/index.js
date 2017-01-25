c65ece4725af9d5965d56d56156dc02124decd97
Aayush Thapa <aayushthapa7@gmail.com>
2017-01-25 19:53:49 +0545

    font and fontSize used

--- src/theme/index.js
+++ src/theme/index.js
@@ -1,5 +1,6 @@
 import Colors from './Colors';
 import Images from './Images';
+import Fonts from './Fonts';
 import ApplicationStyles from './ApplicationStyles';
 
-export { Colors,Images,ApplicationStyles };
+export { Colors,Images,ApplicationStyles,Fonts };
